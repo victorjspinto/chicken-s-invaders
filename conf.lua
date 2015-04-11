b520ec1d777d77fa16ac000a78c05fc8f13f6729
Victor Jose Silva Pinto <victorjosesilvapinto@hotmail.com>
2015-04-11 18:27:58 -0300

    Configs do love para o projeto

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+	t.title = "Chicken`s Invaders" -- The title of the window the game is in (string)
+	t.version = "0.9.2"         -- The LÖVE version this game was made for (string)
+	t.window.width = 800 
+	t.window.height = 600
+
+	-- For Windows debugging
+	t.console = true
+end
