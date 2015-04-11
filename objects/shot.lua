71a8c6860e3f913424c2ad9f874cd633294ef9e9
Victor Jose Silva Pinto <victorjosesilvapinto@hotmail.com>
2015-04-11 14:22:50 -0300

    Agora os tiros estao disponiveis fora do modulo pela property shots Adicionei property size ao tiro, para realizar box colision junto a property size

--- objects/shot.lua
+++ objects/shot.lua
@@ -2,20 +2,20 @@ local shot = {}
 local texture = love.graphics.newImage('assets/sprite-shot.png')
 
 function shot.load()
-	shots = {}
+	shot.shots = {}
 
 	shot.velocity = 100
 end
 
 function shot.update(dt)
-	for i,v in ipairs(shots) do
+	for i,v in ipairs(shot.shots) do
 		v.position.x = v.position.x + v.direction.x * v.velocity * dt
 		v.position.y = v.position.y + v.direction.y * v.velocity * dt
 	end
 end
 
 function shot.draw()
-	for i,v in ipairs(shots) do
+	for i,v in ipairs(shot.shots) do
 		quad = love.graphics.newQuad(2 * 11, 0, 11, 21, texture:getWidth(), texture:getHeight())
 		love.graphics.draw(texture, quad, v.position.x, v.position.y)
 	end
@@ -24,8 +24,12 @@ end
 
 function shot.addShot(position)
 
-	table.insert(shots, {
+	table.insert(shot.shots, {
 		position = position,
+		size = {
+			x = 11,
+			y = 21
+		},
 		direction = {
 			x = 0,
 			y = -1
