b1b87794d3b30841ebb584e97772174e1f1c9a75
Victor Jose Silva Pinto <victorjosesilvapinto@hotmail.com>
2015-04-11 21:33:45 -0300

    Alterei a estrutura q contem posicao e tamanho para padronizar no projeto

--- objects/chicken.lua
+++ objects/chicken.lua
@@ -26,15 +26,32 @@ function chicken.update(dt)
 	end
 end
 
+function chicken.draw()
+
+	for i,v in ipairs(chicken.chickens) do
+		
+		quad = love.graphics.newQuad(v.frame.position * 54, 0, 54, 43, texture:getWidth(), texture:getHeight())
+		love.graphics.draw(texture, quad, v.position.x, v.position.y)
+
+	end
+
+end
+
+
 -- fill the screen with chickens for each new level
-function chicken.loadChickenChalengeLevel ()
-	chickens = {}
+function chicken.loadChickenChalengeLevel()
 
 	for i= 0, 11 do
 		for j= 0, 3 do
-			chicken = {
-				x = 30 + i * 60,
-				y = 30 + j * 50,
+			newChicken = {
+				position = {
+					x = 30 + i * 60,
+					y = 30 + j * 50
+				},
+				size = {
+					x = 54,
+					y = 43
+				},
 				lives = 1,
 				frame = {
 					time = 0,
@@ -44,24 +61,8 @@ function chicken.loadChickenChalengeLevel ()
 				}
 			}
 
-			-- por algum motivo o codigo abaixo não funciona o.O
-			-- chicken.chickens eh iniciado no metodo chicken.load()
-			-- table.insert(chicken.chickens, chicken)
-
-			table.insert(chickens, chicken)
+			table.insert(chicken.chickens, newChicken)
 		end
-	end
-
-	chicken.chickens = chickens
-end
-
-function chicken.draw()
-
-	for i,v in ipairs(chicken.chickens) do
-		
-		quad = love.graphics.newQuad(v.frame.position * 54, 0, 54, 43, texture:getWidth(), texture:getHeight())
-		love.graphics.draw(texture, quad, v.x, v.y)
-
 	end
 
 end
